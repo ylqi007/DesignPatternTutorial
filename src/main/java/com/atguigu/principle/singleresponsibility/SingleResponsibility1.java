c1005fe3fcead5f9d38990487499bc056920af99
ylqi007 <ylqi92121@gmail.com>
2024-02-18 00:26:31 -0800

    单一职责原则 (Single Responsibility Principle)

---
+++ src/main/java/com/atguigu/principle/singleresponsibility/SingleResponsibility1.java
@@ -0,0 +1,21 @@
+package com.atguigu.principle.singleresponsibility;
+
+/**
+ * 方案1分析：
+ *  run()方法违反了单一职责原则。
+ *  解决方案：根据交通工具运行方法的不同，分解成不同类即可。即将A类拆分成A1类和A2类。
+ */
+public class SingleResponsibility1 {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        vehicle.run("摩托车");
+        vehicle.run("汽车");
+        vehicle.run("飞机");
+    }
+}
+
+class Vehicle {
+    public void run(String vehicle) {
+        System.out.println(vehicle + " is running on the road.");
+    }
+}
---
+++ src/main/java/com/atguigu/principle/singleresponsibility/SingleResponsibility3.java
@@ -0,0 +1,29 @@
+package com.atguigu.principle.singleresponsibility;
+
+/**
+ * 方式3分析：
+ *  1. 这种修改方案没有对原有的类做大的修改，只是增加方法
+ *  2. 虽然没有在类的级别上遵守单一职责原则，但是在方法级别上，仍然遵守了单一职责。
+ */
+public class SingleResponsibility3 {
+    public static void main(String[] args) {
+        Vehicle2 vehicle2 = new Vehicle2();
+        vehicle2.runRoad("汽车");
+        vehicle2.runAir("飞机");
+        vehicle2.runWater("游艇");
+    }
+}
+
+class Vehicle2 {
+    public void runRoad(String vehicle) {
+        System.out.println(vehicle + "在公路上运行");
+    }
+
+    public void runAir(String vehicle) {
+        System.out.println(vehicle + "在天空上飞行");
+    }
+
+    public void runWater(String vehicle) {
+        System.out.println(vehicle + "在水中游行");
+    }
+}
